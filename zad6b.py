84038115a2956e79becbcae154262ad0b7aa9a98
JakubJaworek <jakubjaworek1@gmail.com>
2025-10-24 19:35:31 +0200

    update

--- zad6b.py
+++ zad6b.py
@@ -6,5 +6,5 @@ spalanie = float(input("Podaj średnie spalanie w litrach na 100km:"))
 zużycie_paliwa = droga *(spalanie /100)
 koszty = zużycie_paliwa * cena_paliwa
 
-print("Samochód spali:", round(zużycie_paliwa, 2), "litrów")
-print("Koszty wynoszą:", round(koszty), "zł", "na trasie", droga, "km")
+print(f'samochód spali {zużycie_paliwa,2} litrów')
+print(f'Koszty wynoszą:" {koszty} na trasie {droga} km')
